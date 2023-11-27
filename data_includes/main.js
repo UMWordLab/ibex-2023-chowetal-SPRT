7e3649dc44359fe0dcaacad86c478c648fa0f042
lisalevinson <lisalev@umich.edu>
2023-11-27 13:56:45 -0500

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -10,8 +10,11 @@ var shuffleSequence = seq("consent", "IDentry", "demo", "intro",
                             "setcounter",
                             "starter",
 
-                            seq(rshuffle(startsWith("mklo"), startsWith("gp"), startsWith("Subjexp"),
-                                         startsWith("Objexp"))),
+                            // trials named _dummy_ will be excluded by following:
+                            sepWith("sep", 
+                                randomize(anyOf(startsWith("chow"),
+                                startsWith("kutas")))),
+
                             "sendresults",
                             "completion"
                          );
@@ -92,38 +95,38 @@ var manualSendResults = true;
 var defaults = [
 ];
 
-function modifyRunningOrder(ro) {
+// function modifyRunningOrder(ro) {
 
-    var new_ro = [];
-    item_count=0;
-    for (var i in ro) {
-      var item = ro[i];
-      // fill in the relevant experimental condition names on the next line
-      if (item[0].type.startsWith("mklo")|| item[0].type.startsWith("gp")
-            || item[0].type.startsWith("Subjexp") || item[0].type.startsWith("Objexp")) {
-          item_count++;
-          new_ro.push(item);
-        // first number after item count is how many items between breaks. second is total-items - 1
-          if (item_count%30===0 & item_count<86){
-         // value here should be total_items - items_per_block (to trigger message that last block is coming up)
+//     var new_ro = [];
+//     item_count=0;
+//     for (var i in ro) {
+//       var item = ro[i];
+//       // fill in the relevant experimental condition names on the next line
+//       if (item[0].type.startsWith("mklo")|| item[0].type.startsWith("gp")
+//             || item[0].type.startsWith("Subjexp") || item[0].type.startsWith("Objexp")) {
+//           item_count++;
+//           new_ro.push(item);
+//         // first number after item count is how many items between breaks. second is total-items - 1
+//           if (item_count%30===0 & item_count<86){
+//          // value here should be total_items - items_per_block (to trigger message that last block is coming up)
               
-              // NEW: Had to add 3 to get the message to show? I think the message DynamicElement
-              // that is added at the end of this function is increasing the length of RO? Not sure.
-              if (item_count===57 + 3){
-                  text="End of block. Only 1 block left!";
-                  }
-              else {
-        // first number is the total number of blocks. second number is number of items per block
-                  text="End of block. "+(3-(Math.floor(item_count/30)))+" blocks left.";
-              }ro[i].push(new DynamicElement("Message", 
-                                { html: "<p>30-second break - stretch and look away from the screen briefly if needed.</p>" + text, transfer: 30000 }));
-          }
-        } else {
-        new_ro.push(item);
-        }
-    }
-    return new_ro;
-  }
+//               // NEW: Had to add 3 to get the message to show? I think the message DynamicElement
+//               // that is added at the end of this function is increasing the length of RO? Not sure.
+//               if (item_count===57 + 3){
+//                   text="End of block. Only 1 block left!";
+//                   }
+//               else {
+//         // first number is the total number of blocks. second number is number of items per block
+//                   text="End of block. "+(3-(Math.floor(item_count/30)))+" blocks left.";
+//               }ro[i].push(new DynamicElement("Message", 
+//                                 { html: "<p>30-second break - stretch and look away from the screen briefly if needed.</p>" + text, transfer: 30000 }));
+//           }
+//         } else {
+//         new_ro.push(item);
+//         }
+//     }
+//     return new_ro;
+//   }
  
  
 Template("experiment.csv", row => {
@@ -155,6 +158,8 @@ Template("experiment.csv", row => {
        .log("counter", __counter_value_from_server__)
        .log("label", row.label)
        .log("latinitem", row.item)
+       .log("correct_response", row.correct_response)
+       .log("group", row.group)
        ]
    );
    return newTrial('_dummy_',null);
